a2f429fc64f930aa85fc18c19d1aafaf512e00a1
nicko1321 <nickmarino1321@yahoo.com>
2025-06-28 00:14:31 -0700

    Add files via upload

---
+++ client/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1 style={{color: '#00ff99', textAlign: 'center', marginTop: '20%'}}>🚨 Alert 360 AI Dashboard</h1>
+  </React.StrictMode>
+)
